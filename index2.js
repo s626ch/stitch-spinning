baa81ca3a7210be546ed98b986bb1cccf4bfa71c
s626ch <coolguygameplayer52@gmail.com>
2023-05-16 13:15:56 -0400

    yeah

---
+++ index2.js
@@ -0,0 +1,36 @@
+const time = document.querySelector('#time')
+const mainButton = document.querySelector('#starttimer')
+const stopwatch = { elapsedTime: 0 }
+
+mainButton.addEventListener('click', () => {
+    startStopwatch();
+    document.getElementById("stitch").classList.add("stitch")
+})
+
+clearButton.addEventListener('click', () => {
+  stopwatch.elapsedTime = 0
+  stopwatch.startTime = Date.now()
+  displayTime(0, 0, 0, 0)
+})
+
+function startStopwatch() {
+  //reset start time
+  stopwatch.startTime = Date.now();
+  //run `setInterval()` and save id
+  stopwatch.intervalId = setInterval(() => {
+    //calculate elapsed time
+    const elapsedTime = Date.now() - stopwatch.startTime + stopwatch.elapsedTime
+    //calculate different time measurements based on elapsed time
+    const milliseconds = parseInt((elapsedTime%1000)/10)
+    const seconds = parseInt((elapsedTime/1000)%60)
+    const minutes = parseInt((elapsedTime/(1000*60))%60)
+    const hour = parseInt((elapsedTime/(1000*60*60))%24);
+    //display time
+    displayTime(hour, minutes, seconds, milliseconds)
+  }, 100);
+}
+
+function displayTime(hour, minutes, seconds, milliseconds) {
+  const leadZeroTime = [hour, minutes, seconds, milliseconds].map(time => time < 10 ? `0${time}` : time)
+  time.innerHTML = leadZeroTime.join(':')
+}
